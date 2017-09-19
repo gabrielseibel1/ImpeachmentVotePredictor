c8a39ef2772932d272e45e07c152d514ce4c58a9
Gabriel de Souza Seibel <gabrielseibel1@gmail.com>
2017-09-19 10:46:21 -0300

    Implemented solution that uses logarithms. Tests now compare usage or not usage of it

--- src/NaiveBayes.kt
+++ src/NaiveBayes.kt
@@ -1,9 +1,4 @@
-class NaiveBayes {
-
-    /**
-     * Parser to obtain training and testing data
-     */
-    val data = VotesFileParser()
+class NaiveBayes (val data: VotesFileParser) {
 
     /**
      * "P(yes)" : Frequency of class yes divided by total number of training votes
@@ -22,7 +17,12 @@ class NaiveBayes {
         false -> probNo
     }
 
-    fun dropTheBayes(vote: Vote) = predictClass(vote)
+    fun dropTheBayes(vote: Vote, logarithms: Boolean) : Prediction{
+        return when {
+            logarithms -> predictClassLogarithms(vote)
+            else -> predictClass(vote)
+        }
+    }
 
     private fun predictClass(vote: Vote) : Prediction {
         val predictedProbabilities = mutableMapOf(Pair(true, 0.toDouble()), Pair(false, 0.toDouble()))
@@ -38,7 +38,8 @@ class NaiveBayes {
                         timesThatWordAppearsInClass += it.attributes[index]
                     }
 
-                    prod *= timesThatWordAppearsInClass.toDouble() / data.trainingVotes.filter { it.voteClass == ithClass }.size.toDouble()
+                    val pOfWordGivenClass = timesThatWordAppearsInClass.toDouble() / data.trainingVotes.filter { it.voteClass == ithClass }.size.toDouble()
+                    prod *= pOfWordGivenClass
                 }
             }
             prod *= aPrioriProbOfClass(ithClass)
@@ -54,10 +55,43 @@ class NaiveBayes {
         return Prediction(vote, maxProbabilityPair.key, maxProbabilityPair.value)
     }
 
-    fun measureAccuracy() : List<Prediction> {
+    private fun predictClassLogarithms(vote: Vote) : Prediction {
+        val predictedProbabilities = mutableMapOf(Pair(true, 0.toDouble()), Pair(false, 0.toDouble()))
+
+        for (ithClass in predictedProbabilities.keys) {
+            var sum = 0.toDouble()
+
+            for ((index, attribute) in vote.attributes.withIndex()) {
+
+                if (attribute > 0) {
+                    var timesThatWordAppearsInClass = 0
+                    data.trainingVotes.filter { it.voteClass == ithClass }.forEach {
+                        timesThatWordAppearsInClass += it.attributes[index]
+                    }
+
+                    val pOfWordGivenClass = timesThatWordAppearsInClass.toDouble() / data.trainingVotes.filter { it.voteClass == ithClass }.size.toDouble()
+
+                    sum += if (pOfWordGivenClass != 0.0) Math.log(pOfWordGivenClass) else (-999999).toDouble()
+                }
+            }
+            sum += Math.log(aPrioriProbOfClass(ithClass))
+            predictedProbabilities[ithClass] = sum
+        }
+
+        var maxProbabilityPair = predictedProbabilities.entries.elementAt(0)
+        predictedProbabilities.entries
+                .asSequence()
+                .filter { it.value > maxProbabilityPair.value }
+                .forEach { maxProbabilityPair = it }
+
+        return Prediction(vote, maxProbabilityPair.key, maxProbabilityPair.value)
+    }
+
+
+    fun measureAccuracy(logarithms: Boolean) : List<Prediction> {
         val results = mutableListOf<Prediction>()
         data.testingVotes.forEach {
-            results.add(dropTheBayes(it))
+            results.add(dropTheBayes(it, logarithms))
         }
         return results.toList()
     }
--- test/NaiveBayesTest.kt
+++ test/NaiveBayesTest.kt
@@ -3,51 +3,86 @@ import org.junit.jupiter.api.Test
 internal class NaiveBayesTest {
     @Test
     fun probOfClass() {
-        val bayes = NaiveBayes()
+        val bayes = NaiveBayes(VotesFileParser())
         println(bayes.aPrioriProbOfClass(true))
         println(bayes.aPrioriProbOfClass(false))
     }
 
     @Test
     fun dropTheBayes() {
-        val bayes = NaiveBayes()
+        val bayes = NaiveBayes(VotesFileParser())
         var vote = Vote("brasil democracia familia", true)
         vote.buildAttributes(BagOfWords(mutableListOf("Brasil", "Democracia", "familia", "namibia")))
 
-        var prediction = bayes.dropTheBayes(vote)
+        var prediction = bayes.dropTheBayes(vote, logarithms = false)
         println(prediction)
     }
 
     @Test
-    fun accuracyMeasure() {
+    fun dropTheBayesLogarithm() {
+        val bayes = NaiveBayes(VotesFileParser())
+        var vote = Vote("brasil democracia familia", true)
+        vote.buildAttributes(BagOfWords(mutableListOf("Brasil", "Democracia", "familia", "namibia")))
 
+        var prediction = bayes.dropTheBayes(vote, logarithms = true)
+        println(prediction)
+    }
+
+    @Test
+    fun accuracyMeasures() {
         val numberOfBatteries = 50
         val batteriesOfTestsResults = mutableListOf<List<Prediction>>()
         var totalOfPredictions = 0
         var totalOfCorrectPredictions = 0
         var numberOfUncertainPredictions = 0
 
-        (1..numberOfBatteries).forEach {
+        val logarithms_batteriesOfTestsResults = mutableListOf<List<Prediction>>()
+        var logarithms_totalOfPredictions = 0
+        var logarithms_totalOfCorrectPredictions = 0
+        var logarithms_numberOfUncertainPredictions = 0
 
-            val bayes = NaiveBayes()
-            val predictions = bayes.measureAccuracy()
+
+
+        (1..numberOfBatteries).forEach {
+            val bayes = NaiveBayes(VotesFileParser())
+
+            var predictions = bayes.measureAccuracy(logarithms = false)
             totalOfPredictions += predictions.size
             totalOfCorrectPredictions += predictions.filter{ it.isSuccessful()}.size
             numberOfUncertainPredictions += predictions.filter { it.certainty == 0.0 }.size
-
             batteriesOfTestsResults.add(predictions)
+
+            predictions = bayes.measureAccuracy(logarithms = true)
+            logarithms_totalOfPredictions += predictions.size
+            logarithms_totalOfCorrectPredictions += predictions.filter{ it.isSuccessful()}.size
+            logarithms_numberOfUncertainPredictions += predictions.filter { it.certainty >= -999999 }.size
+            logarithms_batteriesOfTestsResults.add(predictions)
         }
 
 
-        println("Ran $numberOfBatteries batteries of tests")
+        println("Ran $numberOfBatteries batteries of tests for each mode - test votes selected at random (10% of all data)")
+
+        println("Mode -> Logarithms = [false] | Laplace = [false]")
         println("///////////////////////////// RESULTS /////////////////////////////")
         println("Success ratio = ${totalOfCorrectPredictions.toDouble()} / ${totalOfPredictions.toDouble()} = " +
                 "${totalOfCorrectPredictions.toDouble() / totalOfPredictions.toDouble()}")
 
         println("${numberOfUncertainPredictions.toDouble() / totalOfPredictions}" +
-                " of the predictions had 0.0 certainty")
+                " of the predictions had low certainty")
         println("${numberOfUncertainPredictions.toDouble() / totalOfCorrectPredictions}" +
-                " of the correct predictions had 0.0 certainty")
+                " of the correct predictions had low certainty")
+        println("///////////////////////////////////////////////////////////////////")
+
+
+        println("Mode -> Logarithms = [true] | Laplace = [false]")
+        println("///////////////////////////// RESULTS /////////////////////////////")
+        println("Success ratio = ${logarithms_totalOfCorrectPredictions.toDouble()} / ${logarithms_totalOfPredictions.toDouble()} = " +
+                "${logarithms_totalOfCorrectPredictions.toDouble() / logarithms_totalOfPredictions.toDouble()}")
+
+        println("${logarithms_numberOfUncertainPredictions.toDouble() / logarithms_totalOfPredictions}" +
+                " of the predictions had low certainty")
+        println("${logarithms_numberOfUncertainPredictions.toDouble() / logarithms_totalOfCorrectPredictions}" +
+                " of the correct predictions had low certainty")
         println("///////////////////////////////////////////////////////////////////")
     }
 }
